4772af003de597724f4b9e47de5abc47edbfdec3
Mihail <67354mishastrel12@gmail.com>
2025-10-31 13:22:47 +0600

    Правильный проект

---
+++ webapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость")
+    image = models.URLField(verbose_name="Ссылка на изображение")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.name
