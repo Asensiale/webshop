f7e7503fda4387cf53b34923f491ba2706683519
Mihail <67354mishastrel12@gmail.com>
2025-11-07 11:50:10 +0600

    Измененный urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('products/add/', views.product_add_view, name='product-add'),
     path('categories/add/', views.category_add_view, name='category-add'),
     path('products/<int:id>/', views.product_detail_view, name='product-detail'),
+    path('products/<int:id>/edit/', views.product_edit_view, name='product-edit'),       # редактирование
+    path('products/<int:id>/delete/', views.product_delete_view, name='product-delete'), # удаление
 ]
+
 
