c665402fe2da8cb42a7b62b10bf9613201528c47
Mihail <67354mishastrel12@gmail.com>
2025-11-07 11:44:35 +0600

    Готовые формы

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from decimal import Decimal
 from .models import Category, Product
 
 class CategoryForm(forms.ModelForm):
@@ -10,15 +11,22 @@ class CategoryForm(forms.ModelForm):
             'description': 'Описание',
         }
 
-
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'category', 'price', 'image']
+        fields = ['name', 'description', 'category', 'price', 'stock', 'image']
         labels = {
             'name': 'Наименование товара',
             'description': 'Описание',
             'category': 'Категория',
             'price': 'Стоимость',
+            'stock': 'Остаток на складе',
             'image': 'Ссылка на изображение',
         }
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price < 0:
+            raise forms.ValidationError("Цена не может быть отрицательной.")
+        if price >= Decimal('100000.00'):
+            raise forms.ValidationError("Максимальная цена: 99999.99")
+        return price
