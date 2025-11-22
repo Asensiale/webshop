bb81603fc0dffb797d37dcc5405dbc8c0dd8d34e
Mihail <67354mishastrel12@gmail.com>
2025-11-22 22:28:44 +0600

    Создана форма ProductForm

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,32 +1,13 @@
 from django import forms
-from decimal import Decimal
-from .models import Category, Product
-
-class CategoryForm(forms.ModelForm):
-    class Meta:
-        model = Category
-        fields = ['name', 'description']
-        labels = {
-            'name': 'Наименование категории',
-            'description': 'Описание',
-        }
+from webapp.models import Product
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'category', 'price', 'stock', 'image']
-        labels = {
-            'name': 'Наименование товара',
-            'description': 'Описание',
-            'category': 'Категория',
-            'price': 'Стоимость',
-            'stock': 'Остаток на складе',
-            'image': 'Ссылка на изображение',
-        }
-    def clean_price(self):
-        price = self.cleaned_data['price']
-        if price < 0:
-            raise forms.ValidationError("Цена не может быть отрицательной.")
-        if price >= Decimal('100000.00'):
-            raise forms.ValidationError("Максимальная цена: 99999.99")
-        return price
+        fields = ["name", "category", "description", "price", "stock", "image"]  # stock вместо amount
+
+    def clean_stock(self):
+        stock = self.cleaned_data.get("stock")
+        if stock < 0:
+            raise forms.ValidationError("Остаток не может быть меньше 0.")
+        return stock
