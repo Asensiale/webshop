a92abefddf2bff90734cfcf8f300356e1850880a
Mihail <67354mishastrel12@gmail.com>
2025-11-23 16:42:14 +0600

    Создана форма OrderForm для оформления заказа с полями: имя, телефон, адрес

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from webapp.models import Product
+from webapp.models import Order
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "category", "description", "price", "stock", "image"]  # stock вместо amount
+        fields = ["name", "category", "description", "price", "stock", "image"]
 
     def clean_stock(self):
         stock = self.cleaned_data.get("stock")
         if stock < 0:
             raise forms.ValidationError("Остаток не может быть меньше 0.")
         return stock
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ["username", "phone", "address"]
