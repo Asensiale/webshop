1e95bbce6fb3a0b00b8ff183ef4b59bab7f5b9c4
Mihail <67354mishastrel12@gmail.com>
2025-11-23 16:45:14 +0600

    Модель Order зарегистрирована в админке с отображением pk, имени, телефона и даты создания Список заказов отсортирован по дате создания по убыванию

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,14 +1,50 @@
 from django.contrib import admin
-from .models import Category, Product
+from webapp.models import (
+    Category,
+    Product,
+    CartProduct,
+    Order,
+    OrderProduct
+)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "description")
     search_fields = ("name",)
+    ordering = ("id",)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "category", "price", "created_at")
+    list_display = ("id", "name", "category", "price", "created_at", "stock")
     list_filter = ("category",)
     search_fields = ("name",)
+    ordering = ("id",)
+
+
+@admin.register(CartProduct)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "quantity")
+    list_filter = ("product",)
+    ordering = ("id",)
+
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "phone", "address", "created_at")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    inlines = [OrderProductInline]
+
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product", "quantity")
+    list_filter = ("order", "product")
+    ordering = ("id",)
